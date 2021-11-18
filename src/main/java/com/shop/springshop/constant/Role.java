2f39fd42467972726cf0ca129bc68b8fcecb2b15
HJin90 <rooriim990@gmail.com>
2021-11-18 15:02:08 +0900

    회원가입 구현

---
+++ src/main/java/com/shop/springshop/constant/Role.java
@@ -0,0 +1,6 @@
+package com.shop.springshop.constant;
+
+public enum Role {
+    USER,
+    ADMIN
+}
---
+++ src/main/java/com/shop/springshop/controller/MemberController.java
@@ -0,0 +1,49 @@
+package com.shop.springshop.controller;
+
+import com.shop.springshop.dto.MemberFormDto;
+import com.shop.springshop.entity.Member;
+import com.shop.springshop.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("/members")
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+    private final PasswordEncoder passwordEncoder;
+
+    @GetMapping("/new")
+    public String memberForm(Model model) {
+        model.addAttribute("memberFormDto", new MemberFormDto());
+        return "member/memberForm";
+    }
+
+    @PostMapping("/new")
+    public String newMember(@Valid MemberFormDto memberFormDto, BindingResult bindingResult, Model model) {
+        if(bindingResult.hasErrors()) {
+            return "member/memberForm";
+        }
+
+        try{
+            Member member = Member.createMember(memberFormDto, passwordEncoder);
+            memberService.saveMember(member);
+        } catch(IllegalStateException e) {
+            model.addAttribute("errorMessage", e.getMessage());
+            return "member/memberForm";
+        }
+
+        return "redirect:/";
+    }
+
+
+}
