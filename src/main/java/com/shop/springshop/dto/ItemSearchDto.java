829bc6aea86cc4be6e6c95a3fff248289fb072ea
HJin90 <rooriim990@gmail.com>
2021-11-25 23:01:32 +0900

    상품 관리페이지 구현, 상품 검색 기능 구현

---
+++ src/main/java/com/shop/springshop/dto/ItemSearchDto.java
@@ -0,0 +1,18 @@
+package com.shop.springshop.dto;
+
+import com.shop.springshop.constant.ItemSellStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ItemSearchDto {
+
+    private String searchDateType;
+
+    private ItemSellStatus searchSellStatus;
+
+    private String searchBy;
+
+    private String searchQuery = "";
+}
