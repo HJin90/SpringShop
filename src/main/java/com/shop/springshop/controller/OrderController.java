c438d436270546907652a24e550cac8df8dcc860
HJin90 <rooriim990@gmail.com>
2021-12-13 15:01:56 +0900

    구매이력 페이지 및 기능 구현 (Order History func and page)

--- src/main/java/com/shop/springshop/controller/OrderController.java
+++ src/main/java/com/shop/springshop/controller/OrderController.java
@@ -1,20 +1,24 @@
 package com.shop.springshop.controller;
 
 import com.shop.springshop.dto.OrderDto;
+import com.shop.springshop.dto.OrderHistoryDto;
 import com.shop.springshop.service.OrderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.security.Principal;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
@@ -44,5 +48,19 @@ public class OrderController {
         }
 
         return new ResponseEntity<Long>(orderId, HttpStatus.OK);
+    }
+
+    @GetMapping(value = {"/orders", "/orders/{page}"})
+    public String orderHistoy(@PathVariable("page") Optional<Integer> page, Principal principal, Model model) {
+
+        Pageable pageable = PageRequest.of(page.isPresent() ? page.get() : 0, 4);
+
+        Page<OrderHistoryDto> orderHistoryDtoList = orderService.getOrderList(principal.getName(), pageable);
+
+        model.addAttribute("orders", orderHistoryDtoList);
+        model.addAttribute("page", pageable.getPageNumber());
+        model.addAttribute("maxPage", 5);
+
+        return "order/orderHistory";
     }
 }
